748efa60ba3d564b7e8ad7b85a5afa97226e6b1e
진주원 <drasgon@naver.com>
2024-10-30 14:19:45 +0900

    feat: S11P31A607-223-Notification-Controller

--- backend/notification-service/build.gradle.kts
+++ backend/notification-service/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
