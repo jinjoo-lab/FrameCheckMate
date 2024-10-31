8987c56eb6847f4657c9d19f936e931a84e8d6af
진주원 <drasgon@naver.com>
2024-10-31 13:16:59 +0900

    S11P31A607-242 feat: Notification TYPE

--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/entity/NotificationType.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/entity/NotificationType.kt
@@ -1,10 +1,11 @@
 package com.framecheckmate.notificationservice.entity
 
-enum class NotificationType {
-    MEMBER_SAVE,
-    TODO,
-    IN_PROGRESS,
-    PENDING_CONFIRMATION,
-    PENDING_CONFIRMATION_REJECTED,
-    COMPLETED
+enum class NotificationType (
+    val koName : String
+) {
+    MEMBER_SAVE("회원가입"),
+    ALLOCATION("작업 배정"),
+    PENDING_CONFIRMATION("컨펌 요청"),
+    PENDING_CONFIRMATION_REJECTED("컨펌 반려"),
+    COMPLETED("최종 승인")
 }
