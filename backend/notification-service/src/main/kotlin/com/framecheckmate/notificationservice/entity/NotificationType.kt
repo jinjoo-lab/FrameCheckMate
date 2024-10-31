9dfc647e90bd021748350853cfebd7ce15507466
진주원 <drasgon@naver.com>
2024-10-31 14:18:07 +0900

    S11P31A607-226 feat: Notification Save Logic

--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/entity/NotificationType.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/entity/NotificationType.kt
@@ -1,11 +1,12 @@
 package com.framecheckmate.notificationservice.entity
 
 enum class NotificationType (
-    val koName : String
+    val koName : String,
+    val typeContent : String
 ) {
-    MEMBER_SAVE("회원가입"),
-    ALLOCATION("작업 배정"),
-    PENDING_CONFIRMATION("컨펌 요청"),
-    PENDING_CONFIRMATION_REJECTED("컨펌 반려"),
-    COMPLETED("최종 승인")
+    MEMBER_SAVE("회원가입","Welcome ~ 회원가입이 완료되었습니다 !"),
+    ALLOCATION("작업 배정","작업 배정이 완료되었습니다"),
+    PENDING_CONFIRMATION("컨펌 요청","작업하신 내용이 컨펌 요청되었습니다"),
+    PENDING_CONFIRMATION_REJECTED("컨펌 반려","관리자에 의해 컨펌 내용이 반려되었습니다"),
+    COMPLETED("최종 승인","관리자에 의해 컨펌 내용이 승인되었습니다")
 }
--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/service/NotificationService.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/service/NotificationService.kt
@@ -1,13 +1,24 @@
 package com.framecheckmate.notificationservice.service
 
 import com.framecheckmate.notificationservice.dto.request.NotificationSaveRequest
+import com.framecheckmate.notificationservice.dto.response.NotificationSaveResponse
+import com.framecheckmate.notificationservice.mapper.NotificationMapper
 import com.framecheckmate.notificationservice.repository.NotificationRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
-class NotificationService(val notificationRepository : NotificationRepository) {
+class NotificationService(
+    val notificationRepository : NotificationRepository,
+    val notificationMapper : NotificationMapper
+) {
 
-    fun saveNotification(notificationSaveRequest: NotificationSaveRequest) {
-
+    @Transactional
+    fun saveNotification(notificationSaveRequest: NotificationSaveRequest) : NotificationSaveResponse {
+           return notificationMapper.entityToSaveResponse(
+               notificationRepository.save(notificationMapper.saveRequestToEntity(notificationSaveRequest))
+           )
     }
+
+
 }
