e8cc86da8c7678ac7232a6055b2373f9b2e6cd5d
김수빈 <86918962+ksb3458@users.noreply.github.com>
2024-10-31 12:59:54 +0900

    S11P31A607-250 refactor: 영상 업로드/다운로드 API

--- backend/card-service/src/main/java/com/framecheckmate/cardservice/domain/frame/controller/FrameController.java
+++ backend/card-service/src/main/java/com/framecheckmate/cardservice/domain/frame/controller/FrameController.java
@@ -5,7 +5,6 @@ import com.framecheckmate.cardservice.domain.frame.service.FrameService;
 import org.springframework.core.io.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,26 +20,16 @@ public class FrameController {
     private final FrameService frameService;
 
     @PostMapping("/upload")
-    public ResponseEntity<FrameUploadResponseDTO> uploadFrame(@RequestBody MultipartFile file) {
-        try {
-            FrameUploadResponseDTO response = frameService.saveFile(file);
-            return ResponseEntity.status(HttpStatus.CREATED).body(response);
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new FrameUploadResponseDTO(null, "파일 업로드 실패: " + e.getMessage()));
-        }
+    public ResponseEntity<FrameUploadResponseDTO> uploadFrame(@RequestBody MultipartFile file) throws IOException {
+        return ResponseEntity.ok(frameService.uploadFrame(file));
     }
 
     @GetMapping("/download")
     // !!RequestParam: CardId로 수정 예정
-    public ResponseEntity<Resource> downloadFrame(@RequestParam String fileName) {
-        try {
-            Resource resource = frameService.loadFileAsResource(fileName);
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                    .body(resource);
-        } catch (MalformedURLException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+    public ResponseEntity<Resource> downloadFrame(@RequestParam String fileName) throws MalformedURLException {
+        Resource resource = frameService.downloadFrame(fileName);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
