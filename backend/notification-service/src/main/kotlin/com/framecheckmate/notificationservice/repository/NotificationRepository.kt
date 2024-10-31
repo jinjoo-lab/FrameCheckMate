5cfbb93e56514c4cb94100c7ea8af59d0c775cea
진주원 <drasgon@naver.com>
2024-10-31 13:42:42 +0900

    S11P31A607-226 feat: Notification Repository

--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/repository/NotificationRepository.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/repository/NotificationRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface NotificationRepository : JpaRepository<Notification,UUID> {
-
+    fun findAllByEmail(email : String) : List<Notification>
 }
