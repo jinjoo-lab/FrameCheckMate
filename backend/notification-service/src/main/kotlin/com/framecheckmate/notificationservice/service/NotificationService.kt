baf64dcb2468c0dbaf90a942f2182693bb2e58ef
진주원 <drasgon@naver.com>
2024-10-31 13:42:49 +0900

    S11P31A607-226 feat: Notification Service
    Former-commit-id: da6eb1e4bfd7af4636afa0d6e67182f7345880da

--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/service/NotificationService.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/service/NotificationService.kt
@@ -1,7 +1,13 @@
 package com.framecheckmate.notificationservice.service
 
+import com.framecheckmate.notificationservice.dto.request.NotificationSaveRequest
+import com.framecheckmate.notificationservice.repository.NotificationRepository
 import org.springframework.stereotype.Service
 
 @Service
-class NotificationService {
+class NotificationService(val notificationRepository : NotificationRepository) {
+
+    fun saveNotification(notificationSaveRequest: NotificationSaveRequest) {
+
+    }
 }
