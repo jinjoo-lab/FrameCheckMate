aef3feeb64e423ac7a2cd08c51a16e741d413654
김수빈 <86918962+ksb3458@users.noreply.github.com>
2024-10-31 18:07:39 +0900

    S11P31A607-247 feat: 영상 업로드 API

--- backend/card-service/src/main/java/com/framecheckmate/cardservice/domain/frame/service/FrameService.java
+++ backend/card-service/src/main/java/com/framecheckmate/cardservice/domain/frame/service/FrameService.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.framecheckmate.cardservice.domain.card.entity.Card;
 import com.framecheckmate.cardservice.domain.frame.entity.Frame;
+import com.framecheckmate.cardservice.domain.frame.entity.FrameLog;
 import com.framecheckmate.cardservice.domain.frame.repository.CardRepository;
 import com.framecheckmate.cardservice.domain.frame.repository.FrameRepository;
 import com.framecheckmate.cardservice.domain.frame.type.FrameType;
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -31,7 +33,29 @@ public class FrameService {
     @Value("${cloud.aws.s3.bucketName}")
     private String bucket;
 
-    public FrameUploadResponseDTO uploadFrame(MultipartFile file) throws IOException {
+    private void uploadCardFrame(UUID cardId, String fileName) {
+        Optional.ofNullable(cardRepository.findByCardId(cardId))
+                .map(Card::getFrameId)
+                .map(frameRepository::findByFrameId)
+                .ifPresent(frame -> {
+                    frame.getLogs().add(new FrameLog(frame.getLogs().size() + 1L, fileName));
+                    frameRepository.save(frame);
+                });
+    }
+
+    private void uploadOriginalFrame(UUID projectId, String fileName) {
+        Frame originalFrame = new Frame(UUID.randomUUID(), projectId, -1L, new ArrayList<>());
+        originalFrame.getLogs().add(new FrameLog(1L, fileName));
+        frameRepository.save(originalFrame);
+    }
+
+    private void uploadMergedFrame(UUID projectId, String fileName) {
+        Frame mergedFrame = frameRepository.findByProjectIdAndSequence(projectId, -1);
+        mergedFrame.getLogs().add(new FrameLog(mergedFrame.getLogs().size() + 1L, fileName));
+        frameRepository.save(mergedFrame);
+    }
+
+    public FrameUploadResponseDTO uploadFrame(UUID id, FrameType type, MultipartFile file) throws IOException {
         String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
 
         ObjectMetadata metadata = new ObjectMetadata();
@@ -39,6 +63,21 @@ public class FrameService {
         metadata.setContentType(file.getContentType());
 
         amazonS3.putObject(bucket, fileName, file.getInputStream(), metadata);
+
+        switch (type) {
+            case CARD:
+                uploadCardFrame(id, fileName);
+                break;
+            case ORIGINAL:
+                uploadOriginalFrame(id, fileName);
+                break;
+            case MERGED:
+                uploadMergedFrame(id, fileName);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid FrameType: " + type);
+        }
+
         return new FrameUploadResponseDTO(amazonS3.getUrl(bucket, fileName).toString(), fileName);
     }
 
