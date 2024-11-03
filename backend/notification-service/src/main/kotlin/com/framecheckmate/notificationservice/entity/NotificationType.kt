7db900ae59e1c66bfdbd15423372702b06fb5451
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-11-03 20:06:04 +0900

    S11P31A607-231 feat: SMTP MAIL TITLE

--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/entity/NotificationType.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/entity/NotificationType.kt
@@ -4,9 +4,9 @@ enum class NotificationType (
     val koName : String,
     val typeContent : String
 ) {
-    MEMBER_SAVE("회원가입","Welcome ~ 회원가입이 완료되었습니다 !"),
-    ALLOCATION("작업 배정","작업 배정이 완료되었습니다"),
-    PENDING_CONFIRMATION("컨펌 요청","작업하신 내용이 컨펌 요청되었습니다"),
-    PENDING_CONFIRMATION_REJECTED("컨펌 반려","관리자에 의해 컨펌 내용이 반려되었습니다"),
-    COMPLETED("최종 승인","관리자에 의해 컨펌 내용이 승인되었습니다")
+    MEMBER_SAVE("FrameCheckMate : [회원가입]","Welcome ~ 회원가입이 완료되었습니다 !"),
+    ALLOCATION("FrameCheckMate : [작업 배정]","작업 배정이 완료되었습니다"),
+    PENDING_CONFIRMATION("FrameCheckMate : [컨펌 요청]","작업하신 내용이 컨펌 요청되었습니다"),
+    PENDING_CONFIRMATION_REJECTED("FrameCheckMate : [컨펌 반려]","관리자에 의해 컨펌 내용이 반려되었습니다"),
+    COMPLETED("FrameCheckMate : [최종 승인]","관리자에 의해 컨펌 내용이 승인되었습니다")
 }
