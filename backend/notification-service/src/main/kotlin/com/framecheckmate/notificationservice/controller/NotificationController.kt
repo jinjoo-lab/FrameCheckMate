c878d55dbf7487b771bd92d510ce4f03bf70d338
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-11-05 21:47:46 +0900

    S11P31A607-359 feat: Spring Cloud Eureka Two
    Former-commit-id: 237fab718a4f1affe2f4f54141c71e4b1cb30254

--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/controller/NotificationController.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/controller/NotificationController.kt
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/notifications")
+@RequestMapping("/notification")
 class NotificationController(
     val notificationService: NotificationService,
 ) {
