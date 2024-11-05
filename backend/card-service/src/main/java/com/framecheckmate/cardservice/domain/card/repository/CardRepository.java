504200ceb0502f48df70880bd0032b5602d4c31c
jooys130 <wndustn130@gmail.com>
2024-11-05 13:45:00 +0900

    S11P31A607-310 feat: Change card list query to filter by projectId
    Former-commit-id: 1b81d4041cd54f3de281a21c8b031a9ade7bc36f

--- backend/card-service/src/main/java/com/framecheckmate/cardservice/domain/card/repository/CardRepository.java
+++ backend/card-service/src/main/java/com/framecheckmate/cardservice/domain/card/repository/CardRepository.java
@@ -6,12 +6,14 @@ import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CardRepository extends MongoRepository<Card, UUID> {
     Card findByCardId(UUID cardId);
+    List<Card> findByProjectId(UUID projectId);
     @Aggregation(pipeline = {
             "{ $match: { 'status': ?0 } }",
             "{ $group: { '_id': null, 'maxOrder': { $max: '$order' } } }"
