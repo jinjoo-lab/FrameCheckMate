7da8737cdcadde0797c87fd971738dbae59298aa
진주원 <drasgon@naver.com>
2024-11-06 16:51:40 +0900

    S11P31A607-332 feat: spring cloud kafka

--- backend/notification-service/build.gradle.kts
+++ backend/notification-service/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.kafka:spring-kafka")
 }
 
 kotlin {
