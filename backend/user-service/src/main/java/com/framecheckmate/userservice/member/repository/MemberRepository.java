f433492158db358fdcf04dd624f31f7ce30a667b
jooys130 <wndustn130@gmail.com>
2024-11-12 14:47:19 +0900

    S11P31A607-418 refactor: Update MemberService

--- backend/user-service/src/main/java/com/framecheckmate/userservice/member/repository/MemberRepository.java
+++ backend/user-service/src/main/java/com/framecheckmate/userservice/member/repository/MemberRepository.java
@@ -4,13 +4,10 @@ import com.framecheckmate.userservice.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface MemberRepository extends JpaRepository<Member, UUID> {
     Member findByEmail(String email);
-    List<Member> findAllByName(String name);
-    List<Member> findAllByEmail(String email);
-
+    List<Member> findAllByNameContainingIgnoreCase(String name);
     boolean existsByEmail(String email);
 }
