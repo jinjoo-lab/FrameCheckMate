12494105045a50ab6e779edda59854248e082df5
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-11-12 18:11:12 +0900

    kafka config

--- backend/card-service/src/main/java/com/framecheckmate/cardservice/config/KafkaProducerConfig.java
+++ backend/card-service/src/main/java/com/framecheckmate/cardservice/config/KafkaProducerConfig.java
@@ -17,7 +17,7 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
 
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "43.203.240.20:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
