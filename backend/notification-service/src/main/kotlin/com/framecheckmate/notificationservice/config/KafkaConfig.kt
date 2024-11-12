3e18b041cb9646e86e6e1cd9dd7acb741c2364c7
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-11-12 16:31:03 +0900

    KafkaConfig

--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/config/KafkaConfig.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/config/KafkaConfig.kt
@@ -12,7 +12,7 @@ class KafkaConfig {
     @Bean
     fun kafkaAdmin(): KafkaAdmin {
         var configs : MutableMap<String, Any> = HashMap()
-        configs[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "127.0.0.1:9092"
+        configs[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = "54.180.247.169:9092"
         return KafkaAdmin(configs)
     }
 
