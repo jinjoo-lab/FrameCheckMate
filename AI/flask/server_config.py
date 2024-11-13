fb5e2767d6fe0132ae945db93a0f1a03fbc39759
진주원 <drasgon@naver.com>
2024-11-13 14:15:47 +0900

    S11P31A607-330 feat: spring-cloud-gateway

--- AI/flask/server_config.py
+++ AI/flask/server_config.py
@@ -1,4 +1,4 @@
 SERVICE_NAME = 'AI-SERVICE'
 SERVICE_PORT = 8083
 SERVICE_HOST = 'host.docker.internal'
-EUREKA_SERVER = 'http://localhost:8761/eureka/'
+EUREKA_SERVER = 'http://k11a607.p.ssafy.io:8761/eureka/'
