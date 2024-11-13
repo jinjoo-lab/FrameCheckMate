0c8af4eab205c694a5cc54199f384ec32989ab21
진주원 <drasgon@naver.com>
2024-11-13 16:40:48 +0900

    S11P31A607-330 feat: spring-card-cors
    Former-commit-id: 2228cff39a9b35131e25b50a7f66ceb127f387be

--- backend/card-service/src/main/java/com/framecheckmate/cardservice/config/WebConfig.java
+++ backend/card-service/src/main/java/com/framecheckmate/cardservice/config/WebConfig.java
@@ -9,11 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000", "https://localhost:3000")
-                .allowedMethods("GET", "POST", "PATCH", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Set-Cookie")
-                .allowCredentials(true);
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("http://localhost:3000", "https://localhost:3000")
+//                .allowedMethods("GET", "POST", "PATCH", "OPTIONS")
+//                .allowedHeaders("*")
+//                .exposedHeaders("Set-Cookie")
+//                .allowCredentials(true);
     }
 }
