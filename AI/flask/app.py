5b79750efc9fd57b4574a067bfff21b0396ce830
김태경 <98coding@gmail.com>
2024-11-14 10:07:49 +0900

    CORS ERROR
    Former-commit-id: 8bff2e888a8895a24f7e42878c1151c4d5757255

--- AI/flask/app.py
+++ AI/flask/app.py
@@ -7,7 +7,7 @@ from py_eureka_client import eureka_client
 import server_config as server
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS"], "allow_headers": ["Content-Type", "Authorization"]}})
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS"], "allow_headers": ["Content-Type", "Authorization", "Access-Control-Allow-Origin"]}})
 
 eureka_client.init(eureka_server=server.EUREKA_SERVER,
                    app_name=server.SERVICE_NAME,
@@ -18,13 +18,19 @@ eureka_client.init(eureka_server=server.EUREKA_SERVER,
 def predict():
     # OPTIONS 요청을 처리하여 프리플라이트 요청에 응답
     if request.method == 'OPTIONS':
-        return jsonify({'status': 'preflight'}), 200
+        # OPTIONS 요청에 대해 허용 응답을 반환
+        response = jsonify({'status': 'OK'})
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+        response.headers.add("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+        return response
 
     # JSON 요청에서 URL 가져오기
     data = request.get_json()
     if not data or 'url' not in data:
+        print('error', ':', 'No URL provided in request')
         return jsonify({'error': 'No URL provided in request'}), 400
-    
+    print(11111)
     video_url = data['url']
     cap = cv2.VideoCapture(video_url)
 
