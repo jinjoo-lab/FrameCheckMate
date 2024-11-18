c8b65e1852af613949287b8d5f5d24dac8442966
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-11-18 16:30:01 +0900

    S11P31A607-477 Kafka Fix
    Former-commit-id: 2e06e3c7af9aaa876b14cb4e5f109f5cf27695f3

--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/config/KafkaConfig.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/config/KafkaConfig.kt
@@ -4,8 +4,11 @@ import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.RetryableTopic
 import org.springframework.kafka.config.TopicBuilder
 import org.springframework.kafka.core.KafkaAdmin
+import org.springframework.kafka.retrytopic.DltStrategy
+import org.springframework.retry.annotation.Backoff
 
 @Configuration
 class KafkaConfig {
@@ -16,6 +19,13 @@ class KafkaConfig {
         return KafkaAdmin(configs)
     }
 
+    @RetryableTopic(
+        attempts = "2",
+        backoff = Backoff(delay = 2000L),
+        dltStrategy = DltStrategy.FAIL_ON_ERROR,
+        dltTopicSuffix = ".dlt",
+        retryTopicSuffix = ".retry"
+    )
     @Bean
     fun makeMemberNotificationTopic() : NewTopic {
         return TopicBuilder.name("member-notification-topic")
@@ -24,6 +34,13 @@ class KafkaConfig {
             .build()
     }
 
+    @RetryableTopic(
+        attempts = "2",
+        backoff = Backoff(delay = 2000L),
+        dltStrategy = DltStrategy.FAIL_ON_ERROR,
+        dltTopicSuffix = ".dlt",
+        retryTopicSuffix = ".retry"
+    )
     @Bean
     fun makeCardNotificationTopic() : NewTopic {
         return TopicBuilder.name("card-notification-topic")
