7182f36fd4e192156f8f947989406257e1097fc1
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-12-07 20:48:46 +0900

    feat : RabbitMQ Add Dead Letter Queue

--- backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/service/NotificationRabbitConsumeService.kt
+++ backend/notification-service/src/main/kotlin/com/framecheckmate/notificationservice/service/NotificationRabbitConsumeService.kt
@@ -2,19 +2,26 @@ package com.framecheckmate.notificationservice.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.framecheckmate.notificationservice.dto.request.NotificationSaveRequest
+import com.framecheckmate.notificationservice.slack.SlackWebHookSender
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Service
 
 @Service
 class NotificationRabbitConsumeService (
     private val notificationService: NotificationService,
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val slackWebHookSender: SlackWebHookSender
 ){
 
     @RabbitListener(queues = ["member-notification-queue", "card-notification-queue"])
     fun consumeMemberNotificationMessage(message : String) {
         val request = convert(message)
         notificationService.sendNotification(request)
+    }
+
+    @RabbitListener(queues = ["member-dead-letter-queue", "card-dead-letter-queue"])
+    fun consumeDeadLetterMessage(message : String) {
+        slackWebHookSender.sendSlackMessage(convert(message))
     }
 
     fun convert(jsonString : String) : NotificationSaveRequest {
